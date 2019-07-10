0a44b1482b71995fa2d35c82d301abc8e1b7253a
Cristiano Dalbem <ninja.dalbem@gmail.com>
2019-07-10 02:13:28 -0300

    Add new custom Light and Dorsia styles

--- src/MapStyleSwitcher.js
+++ src/MapStyleSwitcher.js
@@ -23,8 +23,9 @@ class MapStyleSwitcher extends Component {
                     width: '200px'
                 }}
             >
-                <Option value='mapbox://styles/mapbox/light-v10'>Mapa light</Option>
+                <Option value='mapbox://styles/cmdalbem/cjxseldep7c0a1doc7ezn6aeb'>Mapa light</Option>
                 <Option value='mapbox://styles/mapbox/streets-v11'>Mapa normal</Option>
+                <Option value='mapbox://styles/cmdalbem/cjgmxgkbw000n2rqtucat5zjz'>Dorsia</Option>
                 {/* <Option value='mapbox://styles/mapbox/outdoors-v11'>Outdoors</Option> */}
                 <Option value='mapbox://styles/cmdalbem/cjxsdwb907bfi1cqevxio2bst'>Mapa com satélite</Option>
             </Select>
