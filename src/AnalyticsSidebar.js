68d0c9011fdbb859a63ce8095549e58d52fe11b6
Cristiano Dalbem <ninja.dalbem@gmail.com>
2021-03-07 19:44:12 -0300

    Adds charts and metrics descriptions tooltips

--- src/AnalyticsSidebar.js
+++ src/AnalyticsSidebar.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { Popover } from 'antd';
+import { Popover, Button } from 'antd';
+
+import { PieChart, Pie, Cell } from 'recharts';
 
 import {
     MdInfoOutline as IconInfo,
@@ -32,28 +34,68 @@ class AnalyticsSidebar extends Component {
                 + lengths['calcada-compartilhada'];
         }
 
+        const chartsData = layers
+            .filter(l => 
+                l.id === 'ciclovia' ||
+                l.id === 'ciclofaixa' ||
+                l.id === 'ciclorrota' ||
+                l.id === 'calcada-compartilhada')
+            .map(l => lengths && 
+                {
+                    value: lengths[l.id],
+                    fill: l.style.lineColor
+                }
+            );
+
         return (
             <div
                 className="background-black border-gray-600 border-l bottom-0 fixed h-screen right-0 top-0 w-60 z-10"
                 style={{background: '#211F1C'}}
             >
                 <div className="pt-5 px-4">
-                    <h2 className="">
+                    <h2 className="font-bold">
                         Estatísticas
                     </h2>
 
                     <Section
-                        title="PNB"
-                        description="People near bike"
+                        title="People Near Bike (PNB)"
+                        link={"https://itdpbrasil.org/pnb/"}
+                        description={<>
+                            <p>
+                                Para avaliar as políticas de ciclomobilidade com maior efetividade, o ITDP Brasil apura anualmente um indicador percentual de pessoas que vivem próximas da infraestrutura cicloviária (PNB sigla em inglês para People Near Bike).
+                            </p>
+                            <p>
+                                A partir da rede cicloviária disponível no CicloMapa, o indicador mostra quantas pessoas moram a menos de 300 metros de uma ciclovia, ciclofaixa, ciclorrota ou calçada compartilhada.
+                            </p>
+                        </>}
                     >
                         <BigNum>
                             19%
                         </BigNum>
+
+                        <DataLine
+                            name="Mulheres negras"
+                            length={15}
+                            unit="%"
+                        />
+                        <DataLine
+                            name="Mulheres renda até 1 SM"
+                            length={19}
+                            unit="%"
+                        />
                     </Section>
 
                     <Section
                         title="IDECiclo"
-                        description="IDECiclo"
+                        link="https://ideciclo.ameciclo.org/"
+                        description={<>
+                            <p>
+                                O Índice de Desenvolvimento Cicloviário é uma metodologia para avaliar a malha cicloviária de uma cidade de forma objetiva e replicável, que permite acompanhar a evolução dos parâmetros de qualidade ao longo de uma gestão para uma comparação temporal e intermunicipal. 
+                            </p>
+                            <p>
+                                O índice, desenvolvido pela Ameciclo, assemelha-se ao Copenhagenize Index, iniciativa do escritório dinamarquês Copenheganize, que tenta avaliar e monitorar ao longo do tempo várias cidades do mundo quanto à promoção do uso da bicicleta nestas cidades.
+                            </p>
+                        </>}
                     >
                         <BigNum>
                             0,107
@@ -62,11 +104,43 @@ class AnalyticsSidebar extends Component {
 
                     <Section
                         title="Vias"
-                        description="Vias"
+                        description={<>
+                            <p>
+                                As extensões totais das vias são calculadas automaticamente com base nos dados do OpenStreetMap. 
+                            </p>
+                            <p>
+                                Para vias que tem estrutura dos dois lados nós desenvolvemos um método que automaticamente detecta estes casos e remove esta contagem dupla do total.
+                            </p>
+                            <p className="italic opacity-50 text-xs">
+                                Estes números podem não corresponder à realidade. Eles não são dados oficiais e dependem do estado atual do mapeamento da infraestrutura da cidade no OpenStreetMap e da precisão do nosso método de detecção automática de contagens duplas.
+                            </p>
+                        </>}
                     >
-                        <BigNum>
-                            { totalLength ? Math.round(totalLength) + ' km' : '' }
-                        </BigNum>
+                        <div className="relative">
+                            <PieChart width={207} height={207}>
+                                <Pie
+                                    data={chartsData}
+                                    cx={'50%'} cy={'50%'}
+                                    innerRadius={90} outerRadius={100}
+                                    paddingAngle={4} strokeWidth={0}
+                                    startAngle={90} endAngle={-2700}
+                                />
+                            </PieChart>
+
+                            {
+                                totalLength &&
+                                <div
+                                    className="absolute top-0 w-full flex flex-col items-center justify-center text-xs"
+                                    style={{height: '207px'}}
+                                >
+                                    TOTAL
+                                    <div className="text-4xl font-regular tracking-tighter">
+                                        { Math.round(totalLength)}
+                                    </div> 
+                                    km
+                                </div>
+                            }
+                        </div>
                         
                         <div className="mt-2">
                             {
@@ -76,10 +150,12 @@ class AnalyticsSidebar extends Component {
                                         l.id === 'ciclofaixa' ||
                                         l.id === 'ciclorrota' ||
                                         l.id === 'calcada-compartilhada')
-                                    .map(l => lengths && lengths[l.id] > 0 && 
-                                        <DataLine
+                                    .map(l => 
+                                        <DataLineWithBarChart
                                             name={l.name}
-                                            length={lengths[l.id]}
+                                            length={lengths && lengths[l.id]}
+                                            percent={lengths && lengths[l.id] * 100 / totalLength}
+                                            color={l.style.lineColor}
                                             unit="km"
                                         />
                                     )
@@ -89,7 +165,14 @@ class AnalyticsSidebar extends Component {
 
                     <Section
                         title="Pontos de interesse"
-                        description="Pontos de interesse"
+                        description={<>
+                            <p>
+                                Contagem total dos pontos de interesse do ciclista de cada um dos tipos com que o CicloMapa trabalha.
+                            </p>
+                            <p className="italic opacity-50 text-xs">
+                                Estes números podem não corresponder à realidade. Eles não são dados oficiais e dependem do estado atual do mapeamento da infraestrutura da cidade no OpenStreetMap.
+                            </p>
+                        </>}
                     >
                         {
                             layers
@@ -110,8 +193,24 @@ class AnalyticsSidebar extends Component {
 }
 
 const BigNum = ({children}) =>
-    <div className="text-5xl font-regular tracking-tighter">
+    <div className="text-5xl font-regular tracking-tighter -mt-2 mb-1">
         { children }
+    </div>
+
+const DataLineWithBarChart = (props) =>
+    <div className="mb-2">
+        <DataLine {...props}/>
+
+        <div className="w-full h-1 relative bg-white bg-opacity-10 mt-1">
+            <div 
+                className="h-1"
+                style={{ 
+                    transition: 'width 1500ms ease',
+                    background: props.color,
+                    width: (props.percent || 0) + '%'
+                }}> 
+            </div>
+        </div>
     </div>
 
 const DataLine = (props) =>
@@ -121,7 +220,7 @@ const DataLine = (props) =>
         </span>
 
         <span>
-            { Math.round(props.length) }
+            { props.length && Math.round(props.length) }
             { props.unit && ' ' + props.unit }
         </span>
     </div>
@@ -132,12 +231,23 @@ const Section = (props) =>
             placement="left"
             arrowPointAtCenter={true} key={props.title}
             content={(
-                <div style={{width: 300}}>
+                <div style={{width: 320}}>
+                    <h3 className="text-lg">
+                        { props.title }
+                    </h3>
+                    
                     { props.description }
+
+                    {
+                        props.link &&
+                        <Button ghost size="small" target="_BLANK" href={props.link}>
+                            Saiba mais
+                        </Button> 
+                    }
                 </div>
             )}
         >
-            <div className="flex w-100 justify-between items-center cursor-pointer opacity-50">
+            <div className="flex w-full justify-between items-center cursor-pointer opacity-50">
                 <h3 className="font-regular m-0">
                     { props.title }
                 </h3>
