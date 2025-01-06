b9b0a11eeb8efcc74e0e448ce528f7563491f8bf
Cristiano Dalbem <ninja.dalbem@gmail.com>
2025-01-06 11:41:25 +0100

    Fix new React root API

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client'; // Note the updated import
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
 import { BrowserRouter as Router } from "react-router-dom";
 
-ReactDOM.render(
-  <Router>
-    <App ref={(app) => { window.ciclomapa = app }}/>
-  </Router>,
-  document.getElementById('root')
+// Create a root using ReactDOM.createRoot
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Render the app using the new root API
+root.render(
+  <React.StrictMode>
+    <Router>
+      <App ref={(app) => { window.ciclomapa = app }} />
+    </Router>
+  </React.StrictMode>
 );
 
 // If you want your app to work offline and load faster, you can change
