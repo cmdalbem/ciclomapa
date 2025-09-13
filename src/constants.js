1d407c201bea81a936bb5d8718d3de0f59a75ded
Cristiano Dalbem <ninja.dalbem@gmail.com>
2025-09-13 12:09:26 +0200

    Shows which side of the road the cyclepath is / makes all cyclepaths 2x thinner / Decrease a little POI circles

--- src/constants.js
+++ src/constants.js
@@ -38,14 +38,11 @@ export const GRAPHHOPPER_API_KEY = process.env.REACT_APP_GRAPHHOPPER_API_KEY;
 // Layers
 
 export const DEFAULT_BORDER_WIDTH = 3;
-export const DEFAULT_LINE_WIDTH_MULTIPLIER = 2;
+export const DEFAULT_LINE_WIDTH_MULTIPLIER = 1;
 export const LINE_WIDTH_MULTIPLIER_HOVER = 2;
 
 export const DIRECTIONS_LINE_WIDTH = 24;
 export const DIRECTIONS_LINE_BORDER_WIDTH = 4;
-
-export const ROUTES_ACTIVE_OPACITY = 0.4;
-
 
 // Map
 
