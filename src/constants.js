057ab3cca9835965bdd01321d0a298611fe5b464
Cristiano Dalbem <ninja.dalbem@gmail.com>
2025-09-23 11:04:55 +0200

    New dynamic zoom thresholds to POIs

--- src/constants.js
+++ src/constants.js
@@ -56,7 +56,6 @@ export const DEFAULT_AREA = 'Fortaleza, Ceará, Brasil';
 export const DEFAULT_LNG = -38.5225359;
 export const DEFAULT_LAT = -3.7719909;
 export const DEFAULT_ZOOM = 12;
-export const POI_ZOOM_THRESHOLD = 15;
 export const INTERACTIVE_LAYERS_ZOOM_THRESHOLD = 12;
 export const COMMENTS_ZOOM_THRESHOLD = 13;
 export const MAP_AUTOCHANGE_AREA_ZOOM_THRESHOLD = 11;
