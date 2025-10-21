9e1e6cfe4ac32ebd42557729941c53584dc51f41
Cristiano Dalbem <ninja.dalbem@gmail.com>
2025-10-21 21:26:30 +0200

    Improvements on ABout modal

--- src/AboutModal.js
+++ src/AboutModal.js
@@ -19,21 +19,21 @@ class AboutModal extends Component {
                     ${visible ? 'opacity-100' : 'opacity-0 pointer-events-none'} transition-opacity duration-500
                 `}
                 style={{
-                    background: 'rgba(37,32,29,0.9)',
+                    background: 'rgba(37,32,29,0.95)',
                     color: 'white'
                 }}
             >
                 <div className="relative max-w-lg p-4 sm:p-0">
-                    <div className="text-small md:text-lg tracking-wide">
-                        <img className="h-12 mb-8" src="logo.svg" alt="CicloMapa"></img>
+                    <div className="mt-12">
+                        <img className="h-10 mb-8" src="logo.svg" alt="CicloMapa"></img>
 
-                        <div className="mb-8">
+                        <div className="mb-8 text-sm md:text-lg">
                             <p>
-                                O CicloMapa é uma ferramenta que busca ampliar a visibilidade das infraestruturas cicloviárias (ou a falta delas) das cidades brasileiras, disponibilizando dados que auxiliem a incidência em políticas públicas para a ciclomobilidade.
+                                O CicloMapa é uma plataforma colaborativa gratuita que mostra onde existem (e onde faltam) ciclovias, ciclofaixas, ciclorrotas e outros equipamentos para bicicletas nas cidades brasileiras. 
                             </p>
 
                             <p>
-                                Utilizando o <a className="underline" href="https://www.openstreetmap.org/" title="OpenStreetMap" target="_blank" rel="noopener noreferrer">OpenStreetMap (OSM)</a>, uma plataforma colaborativa de mapeamento de dados geoespaciais abertos, também procura engajar a comunidade em sua atualização e na importância de dados abertos.
+                                Usando os dados do <a className="underline" href="https://www.openstreetmap.org/" title="OpenStreetMap" target="_blank" rel="noopener noreferrer">OpenStreetMap (OSM)</a>  — que qualquer pessoa pode editar — ela permite que cidadãos, pesquisadores e gestores públicos visualizem, baixem e usem essas informações para melhorar a mobilidade por bicicleta no Brasil.
                             </p>
                         </div>
 
@@ -42,49 +42,46 @@ class AboutModal extends Component {
                         </Button>
 
                         <div className="mt-16">
-                            <div className="flex items-start justify-between opacity-80">
-                                <div className="w-8/12">
-                                    <div className='flex h-10 flex-row mb-6'>
-                                        <a className="h-full mr-4"
-                                            href="https://www.uniaodeciclistas.org.br/"
-                                            arget="_BLANK" rel="noopener noreferrer"
-                                        >
-                                            <img className="h-full" src={ucb} alt="Logo da UCB" />
-                                        </a>
-                                        <a className="h-full mr-4"
-                                            href="https://itdpbrasil.org/"
-                                            target="_BLANK" rel="noopener noreferrer"
-                                        >
-                                            <img className="h-full" src={itdp} alt="Logo do ITDP" />
-                                        </a>
-                                    </div>
+                            <hr className="border-black border-opacity-20"/>
 
-                                    <p className="text-xs font-semibold mb-6">
-                                        Design & desenvolvimento por  <a className="underline" href="https://www.cristianodalbem.com/" target="_BLANK" rel="noopener noreferrer" > Cristiano Dalbem </a>
-                                    </p>
-
-                                    <div className='flex h-10 flex-row'>
-                                        <a className="h-full"
-                                            href="https://www.gov.br/cidades/pt-br/acesso-a-informacao/acoes-e-programas/mobilidade-urbana/programa-bicicleta-brasil/premio-bicicleta-brasil/"
-                                            target="_BLANK" rel="noopener noreferrer"
-                                        >
-                                            <img className="h-full" src={premiobicicletabrasil} alt="Logo do premiobicicletabrasil" />
-                                        </a>
-                                    </div>
-                                </div>
-                                <div className="w-4/12 flex flex-col">
-                                    <a className="underline" href="https://www.uniaodeciclistas.org.br/atuacao/ciclomapa/" title="CicloMapa | UCB - União de Ciclistas do Brasil" target="_blank" rel="noopener noreferrer">
-                                        Tutoriais
-                                    </a>
-                                    <a className="underline" href="mailto:contato@ciclomapa.org.br" target="_blank" rel="noopener noreferrer">
-                                        Contato
-                                    </a>
-
-                                    <a className="underline" href="https://github.com/cmdalbem/ciclomapa/" title="GitHub" target="_blank" rel="noopener noreferrer">
-                                        GitHub
-                                    </a>
-                                </div>
+                            <div className='flex h-10 mt-6 mb-6 gap-4 justify-between'>
+                                <a className="h-full b-and-w"
+                                    href="https://itdpbrasil.org/"
+                                    target="_BLANK" rel="noopener noreferrer"
+                                >
+                                    <img className="h-full" src={itdp} alt="Logo do ITDP" />
+                                </a>
+                                <a className="h-full b-and-w"
+                                    href="https://www.gov.br/cidades/pt-br/acesso-a-informacao/acoes-e-programas/mobilidade-urbana/programa-bicicleta-brasil/premio-bicicleta-brasil/"
+                                    target="_BLANK" rel="noopener noreferrer"
+                                >
+                                    <img className="h-full" src={premiobicicletabrasil} alt="Logo do premiobicicletabrasil" />
+                                </a>
+                                <a className="h-full b-and-w"
+                                    href="https://www.uniaodeciclistas.org.br/"
+                                    target="_BLANK" rel="noopener noreferrer"
+                                >
+                                    <img className="h-full" src={ucb} alt="Logo da UCB" />
+                                </a>
                             </div>
+
+                            <hr className="border-black border-opacity-20"/>
+
+                            <p className="flex gap-8 mt-6 mb-6 justify-center">
+                                <a className="underline" href="https://www.uniaodeciclistas.org.br/atuacao/ciclomapa/" title="CicloMapa | UCB - União de Ciclistas do Brasil" target="_blank" rel="noopener noreferrer">
+                                    Tutoriais
+                                </a>
+                                <a className="underline" href="mailto:contato@ciclomapa.org.br" target="_blank" rel="noopener noreferrer">
+                                    Contato
+                                </a>
+                                <a className="underline" href="https://github.com/cmdalbem/ciclomapa/" title="GitHub" target="_blank" rel="noopener noreferrer">
+                                    GitHub
+                                </a>
+                            </p>
+
+                            <p className="text-xs mt-6 mb-6 opacity-50 text-center">
+                                design & desenvolvimento por  <a className="underline" href="https://www.cristianodalbem.com/" target="_BLANK" rel="noopener noreferrer" > Cristiano Dalbem </a>
+                            </p>
                         </div>
                     </div>
                 </div>
