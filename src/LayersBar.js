929f0ef24c4020fb5c0bca62014f6aade8e29647
Cristiano Dalbem <ninja.dalbem@gmail.com>
2025-10-26 13:27:44 +0100

    Create new layer legends modal

--- src/LayersBar.js
+++ src/LayersBar.js
@@ -3,6 +3,7 @@ import { IS_MOBILE, TOPBAR_HEIGHT } from './constants.js'
 
 import bikeparkingIcon from './img/icons/poi-bikeparking-mini.png';
 import bikeparkingIconLight from './img/icons/poi-bikeparking-mini--light.png';
+import { HiOutlineInformationCircle } from 'react-icons/hi';
 
 class LayersBar extends Component {
     constructor(props) {
@@ -128,7 +129,7 @@ class LayersBar extends Component {
 
         const background = style.lineStyle === 'solid' 
             ? style.lineColor
-            : `repeating-linear-gradient(90deg, ${style.lineColor}, ${style.lineColor} 3px, transparent 3px, transparent 5px)`;
+            : `repeating-linear-gradient(90deg, ${style.lineColor}, ${style.lineColor} 4px, transparent 3px, transparent 6px)`;
         
         return {
             background,
@@ -159,7 +160,7 @@ class LayersBar extends Component {
                         <img className="w-3 h-3" src={icon} alt="" />
                     ) : lineStyle ? (
                         <span 
-                            className="w-3 h-1 rounded"
+                            className="w-4 h-1 rounded"
                             style={this.renderLineStyle(lineStyle)}
                         />
                     ) : null}
@@ -298,6 +299,14 @@ class LayersBar extends Component {
                             });
                         });
                     })()}
+
+                    {/* Legend info icon */}
+                    <button
+                        onClick={this.props.openLayersLegendModal}
+                        className="flex items-center justify-center ml-2 px-2 py-2 rounded-full text-xs transition-all duration-200 glass-bg flex-shrink-0"
+                    >
+                        <HiOutlineInformationCircle className="text-base" />
+                    </button>
                 </div>
             </div>
         );
