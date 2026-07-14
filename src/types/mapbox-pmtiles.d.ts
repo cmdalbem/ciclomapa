fc87ece139d296cae08ec5c0a91ca8de3bd3db1d
Cristiano Dalbem <cristiano@ashbyhq.com>
2026-07-14 23:06:12 +0200

    Migrate pmtiles support to the new official Mapbox support, enabling again symbol layers to use pmtiles sources.
    Also fix the USE_GEOJSON_SOURCE constant
    
    (cherry picked from commit 21df78648505d1f563e4afa25c5b57945325982c)

--- src/types/mapbox-pmtiles.d.ts
+++
@@ -1,5 +0,0 @@
-declare module 'mapbox-pmtiles' {
-  // `mapbox-pmtiles` ships TypeScript sources and can fail strict typechecking
-  // inside node_modules. We intentionally treat it as untyped at the app layer.
-  export const PmTilesSource: any;
-}
